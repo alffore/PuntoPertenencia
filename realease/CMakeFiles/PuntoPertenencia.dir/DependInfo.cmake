a3394181d0f5c09e22785405b35ce035af37471a
alffore <alffore@gmail.com>
2025-08-24 14:56:32 -0600

    limpieza de codigos

---
+++ realease/CMakeFiles/PuntoPertenencia.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alfonso/devel/PuntoPertenencia/src/coordenadas/CCG2CCL.cpp" "CMakeFiles/PuntoPertenencia.dir/src/coordenadas/CCG2CCL.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/coordenadas/CCG2CCL.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/coordenadas/ITRF2CCL.cpp" "CMakeFiles/PuntoPertenencia.dir/src/coordenadas/ITRF2CCL.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/coordenadas/ITRF2CCL.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/lectores/LectorAgeb.cpp" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorAgeb.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorAgeb.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/lectores/LectorEnt.cpp" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorEnt.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorEnt.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/lectores/LectorINT.cpp" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorINT.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorINT.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/lectores/LectorMnz.cpp" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorMnz.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorMnz.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/lectores/LectorMun.cpp" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorMun.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorMun.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/lectores/LectorRec.cpp" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorRec.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/lectores/LectorRec.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/main.cpp" "CMakeFiles/PuntoPertenencia.dir/src/main.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/main.cpp.o.d"
+  "/home/alfonso/devel/PuntoPertenencia/src/salidas.cpp" "CMakeFiles/PuntoPertenencia.dir/src/salidas.cpp.o" "gcc" "CMakeFiles/PuntoPertenencia.dir/src/salidas.cpp.o.d"
+  "" "PuntoPertenencia" "gcc" "CMakeFiles/PuntoPertenencia.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
